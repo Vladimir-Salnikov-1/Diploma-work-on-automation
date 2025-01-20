b659014d6ba3330dd84c46394f8dfc333e638267
unknown <ya-vladimir-salnikov.ru>
2025-01-20 13:08:26 +0300

    1 добавил минимальный список тестов, и первоначальные переменные в классах

---
+++ Classes/DataForTests.py
@@ -0,0 +1,12 @@
+class DataForTests:
+    def __init__(self):
+        pass
+
+    access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.\
+        eyJleHAiOjE3Mjg0NzE1MDIsImlhdCI6MTcyODMwMzUwMiwia\
+            XNzIjoiL2FwaS92MS9hdXRoL2Fub255bW91cyIsInN1Yi\
+                I6ImExYTcyNzcxMWY2NjIzZmRlMGU3YmFjZjVhMTll\
+                    NWYyM2RhNzA2ZDRhZmQ3NWIxODU5ZjI2YTg3MGF\
+                        lMzNjOGUiLCJ0eXBlIjoxMH0.NSi_-bfYwkt\
+                            mLtOJOeuaAIMaZj_NnwBO3KkH9JgHWLQ"
+    base_url = "https://web-gate.chitai-gorod.ru/api/"
