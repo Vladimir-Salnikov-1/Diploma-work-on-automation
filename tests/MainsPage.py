3f5d43d7854335a889fd9b11b515fb3816a2d8a5
Vladimir-Salnikov-1 <ya.vladimir-salnikov.ru>
2025-01-27 19:24:53 +0300

    15 завернул селекторы в переменные, чуть поправил методы для тестов ui

--- tests/MainsPage.py
+++ tests/MainsPage.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+from selenium.common.exceptions import TimeoutException
 import re
 
 
@@ -16,64 +17,81 @@ selector_anim_load_spin_in_input = ".chg-app-loader"
 selector_element_close_input = "[aria-label='Очистить']"
 selector_element_search_button = ".header-search__button"
 selector_messages_about_found_products = ".search-page__found-message"
+selector_element_message_on_search = ".search-page__found-message"
+selector_element_products_list = ".products-list"
+selector_element_product_cart = "article"
+selector_element_button_of_item = ".button.action-button.blue"
+selector_element_title_product = ".product-title__head"
+selector_container_empty_result = ".catalog-empty-result__container"
+selector_header_empty_result = ".catalog-empty-result__header"
+selector_counter_on_cart_icon = ".header-cart__badge"
+
 
 class MainsPage:
-    
+
     def __init__(self):
         pass
-        browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        browser = webdriver.Chrome(
+            service=ChromeService(ChromeDriverManager().install()))
         self.browser = browser
-        
+
     def go_to_main_page(self):
         """Этот метод переходит на главную страницу сайта"""
         self.browser.get(DataForTests.base_url_for_ui)
-        
+
     def send_keys_input(self, name: str):
         """Этот метод вводит в полле ввода поиска какое-то значение.
         На вход принимает название товара или автора"""
-        elem_input = self.browser.find_element(By.CSS_SELECTOR, selector_element_input)
+        elem_input = self.browser.find_element(
+            By.CSS_SELECTOR, selector_element_input)
         elem_input.clear()
         elem_input.send_keys(name)
-        WebDriverWait(self.browser, 10, 0.1).until(
-            EC.visibility_of_element_located(
-                (By.CSS_SELECTOR, selector_anim_load_spin_in_input)))
+        try:
+            WebDriverWait(self.browser, 10, 0.1).until(
+                EC.visibility_of_element_located(
+                    (By.CSS_SELECTOR, selector_anim_load_spin_in_input)))
+        except TimeoutException:
+            print("Спиннер загрузки не появился в течение указанного времени.")
         WebDriverWait(self.browser, 10, 0.1).until(
             EC.element_to_be_clickable(
                 (By.CSS_SELECTOR, selector_element_close_input)))
-        
-        
+
     def push_button_search(self):
-        elem_button_search = self.browser.find_element(By.CSS_SELECTOR, selector_element_search_button)
+        elem_button_search = self.browser.find_element(
+            By.CSS_SELECTOR, selector_element_search_button)
         elem_button_search.click()
         WebDriverWait(self.browser, 10, 0.1).until(
             EC.visibility_of_all_elements_located(
-                (By.CSS_SELECTOR, ".product-title__head")))
-       
-    
+                (By.CSS_SELECTOR, selector_element_title_product)))
+
     def get_elements_result_item(self):
-        elems = self.browser.find_elements(By.CSS_SELECTOR, selector_elements_result_item)
-            
+        elems = self.browser.find_elements(
+            By.CSS_SELECTOR, selector_elements_result_item)
         return elems
-    
+
     def get_list_name_of_result_search(self):
-        list_name = self.browser.find_elements(By.CSS_SELECTOR, ".product-title__head")
+        list_name = self.browser.find_elements(
+            By.CSS_SELECTOR, selector_element_title_product)
         return list_name
-    
+
     def push_button_search_with_unknown_product(self):
-        elem_button_search = self.browser.find_element(By.CSS_SELECTOR, selector_element_search_button)
+        elem_button_search = self.browser.find_element(
+            By.CSS_SELECTOR, selector_element_search_button)
         elem_button_search.click()
         WebDriverWait(self.browser, 10, 0.1).until(
             EC.visibility_of_all_elements_located(
-                (By.CSS_SELECTOR, ".catalog-empty-result__container")))
-        
+                (By.CSS_SELECTOR, selector_container_empty_result)))
+
     def get_elements_result_search_unknown_product(self):
-        res = self.browser.find_element(By.CSS_SELECTOR, ".catalog-empty-result__header")
+        res = self.browser.find_element(
+            By.CSS_SELECTOR, selector_header_empty_result)
         return res.text
-    
+
     def get_value_from_cart_icon(self):
-        res = self.browser.find_element(By. CSS_SELECTOR, ".header-cart__badge")
+        res = self.browser.find_element(
+            By. CSS_SELECTOR, selector_counter_on_cart_icon)
         return res.text
-    
+
     def get_value_search_from_found_message(self):
         """Этот метод возвращает значение запроса
         из сообщения о найденных товарах"""
@@ -82,8 +100,24 @@ class MainsPage:
         match = re.search(r"«(.*?)»", res)
         value_search_from_found_message = match.group(1)
         return value_search_from_found_message
-         
+
     def get_buttons_buy(self):
-        elements = self.browser.find_elements(By.CSS_SELECTOR, ".button.action-button.blue")
+        elements = self.browser.find_elements(
+            By.CSS_SELECTOR, selector_element_button_of_item)
         buy_buttons = [el for el in elements if el.text.strip() == "КУПИТЬ"]
         return buy_buttons
+
+    def get_product_carts(self) -> list:
+        elements = self.browser.find_elements(
+            By.CSS_SELECTOR, selector_element_product_cart)
+        return elements
+
+    def get_list_of_products(self) -> str:
+        products_list_before = self.browser.find_element(
+            By. CSS_SELECTOR, selector_element_products_list).text
+        return products_list_before
+
+    def get_message_on_the_search_results(self) -> str:
+        message = self.browser.find_element(
+            By. CSS_SELECTOR, selector_element_message_on_search).text
+        return message
--- tests/test_ui.py
+++ tests/test_ui.py
@@ -8,13 +8,8 @@ from DataForTests import DataForTests
 from time import sleep
 from MainsPage import MainsPage
 
-long_name = "В целом, конечно, укрепление и развитие внутренней структуры\
-    говорит о возможностях новых принципов формирования материально-\
-        технической и кадровой базы. Разнообразный и богатый опыт говорит нам,\
-            что укрепление и развитие внутренней структуры создаёт\
-                необходимость включения в производственный план целого ряда\
-                    внеочередных мероприятий с учётом комплекса новых\
-                        предложений."
+long_name_373 = "В целом, конечно, укрепление и развитие внутренней структуры говорит о возможностях новых принципов формирования материально-технической и кадровой базы. Разнообразный и богатый опыт говорит нам, что укрепление и развитие внутренней структуры создаёт необходимость включения в производственный план целого ряда внеочередных мероприятий с учётом комплекса новых предложений."
+
 
 def test_result_search():
     main_page = MainsPage()
@@ -37,6 +32,7 @@ def test_dropdown_list_results_match_the_entered_value():
     name_1 = res[0].text
     assert len(res) == 4
     assert name.lower() in name_1.lower()
+    main_page.browser.quit()
 
 
 def test_search_for_an_unknown_product():
@@ -47,6 +43,7 @@ def test_search_for_an_unknown_product():
     main_page.push_button_search_with_unknown_product()
     elem = main_page.get_elements_result_search_unknown_product()
     assert "Похоже, у нас такого нет" in elem
+    main_page.browser.quit()
 
 
 def test_add_items_in_cart():
@@ -64,6 +61,7 @@ def test_add_items_in_cart():
                 (By.CSS_SELECTOR, ".header-cart__badge")))
     res = main_page.get_value_from_cart_icon()
     assert int(res) == 1
+    main_page.browser.quit()
 
 
 def test_value_in_input_so_value_in_result():
@@ -74,47 +72,41 @@ def test_value_in_input_so_value_in_result():
     main_page.push_button_search()
     value = main_page.get_value_search_from_found_message()
     assert value == name
-    
+    main_page.browser.quit()
+
+
 def test_negitive_very_long_value_in_input():
     main_page = MainsPage()
     main_page.go_to_main_page()
-    main_page.send_keys_input(long_name)
+    main_page.send_keys_input(long_name_373)
     WebDriverWait(main_page.browser, 10, 0.1).until(
-            EC.visibility_of_element_located(
-                (By.CSS_SELECTOR, ".result-item__mark")))
+        EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, ".result-item__mark")))
     elem = main_page.browser.find_element(
         By.CSS_SELECTOR, ".result-item__mark").text
     len_elem = len(elem)
-    assert len_elem == 139
-    
+    assert len_elem == 150, "длина текста не соответствует"
+    main_page.browser.quit()
+
+
 def test_some_click_on_button_search():
     main_page = MainsPage()
     main_page.go_to_main_page()
     name = "апофения"
     main_page.send_keys_input(name)
     main_page.push_button_search()
-    res_before = main_page.browser.find_element(
-            By. CSS_SELECTOR, ".search-page__found-message").text
-    len_item_before = len(main_page.browser.find_elements(By.CSS_SELECTOR, "article"))
-    products_list_before = main_page.browser.find_element(By. CSS_SELECTOR, ".products-list").text
-    assert name in res_before
+    message_before = main_page.get_message_on_the_search_results()
+    len_item_before = len(main_page.get_product_carts())
+    products_list_before = main_page.get_list_of_products()
+    assert name in message_before
+    main_page.browser.quit()
     
     for push in range(11):
         main_page.push_button_search()
-    res_after = main_page.browser.find_element(
-            By. CSS_SELECTOR, ".search-page__found-message").text
-    len_item_after = len(main_page.browser.find_elements(By.CSS_SELECTOR, "article"))
-    products_list_after = main_page.browser.find_element(By. CSS_SELECTOR, ".products-list").text
-    assert res_before == res_after, "Сообщение о результате поиска поменялось"
+    message_after = main_page.get_message_on_the_search_results()
+    len_item_after = len(main_page.get_product_carts())
+    products_list_after = main_page.get_list_of_products()
+    assert message_before == message_after, "Сообщение о результате поиска поменялось"
     assert len_item_before == len_item_after, "Добавились новые товары"
     assert products_list_before == products_list_after
-    
-    # WebDriverWait(self.browser, 10, 0.1).until(
-    #     EC.visibility_of_element_located(
-    #         (By.CSS_SELECTOR, selector_anim_load_spin_in_input)))
-    # WebDriverWait(self.browser, 10, 0.1).until(
-    #     EC.element_to_be_clickable(
-    #         (By.CSS_SELECTOR, selector_element_close_input)))
-    
-
-    
+    main_page.browser.quit()
